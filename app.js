11fb58dbe9abcb4cb0b9f7cd2968d61cb440169d
Nadiah Mauricio <nadiahmauricio@gmail.com>
2023-12-04 15:26:00 +0000

    copy code jr

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
+import "./style.css";
+
 const currencyValues = {
   EUR: 1,
   USD: 1.07,
   GBP: 0.87,
 };
+
 const currencyToSymbol = {
   EUR: "€",
   USD: "$",
@@ -10,107 +13,158 @@ const currencyToSymbol = {
 };
 
 const tableBodyNode = document.querySelector("#table-body");
-const newOderForm = document.querySelector("#new-order-form");
+const newOrderForm = document.querySelector("#new-order-form");
 const addProductBtn = document.querySelector("#add-product");
+const productsForm = document.querySelector("#order-products-form");
 
+const ordersPromise = fetch("http://116.203.151.6:3000/orders");
+const productsPromise = fetch("http://116.203.151.6:3000/products");
 
-
-
-const ordersPromise = fecth ("http://116.203.151.6:3000/orders")
-
-const productsPromise = fecth ("http://116.203.151.6:3000/products");
-
-const [ordersResponde, productsResponse] = await Promise.all([ordersPromise, productsPromise]);
-const [orders, products] = await Promise.all([ordersResponde.jason(), productsResponse.jason()]);
+const [ordersResponse, productsResponse] = await Promise.all([
+  ordersPromise,
+  productsPromise,
+]);
+const [orders, products] = await Promise.all([
+  ordersResponse.json(),
+  productsResponse.json(),
+]);
 
 for (const order of orders) {
   const orderCurrency = order.currency;
 
   let orderPrice = 0;
 
-  for (const productFromOrder of orders.products) {
-    const foundProduct = products.find((productFromDb) => productFromOrder.name === productFromDb.name);
+  for (const productFromOrder of order.products) {
+    const foundProduct = products.find(
+      (productFromDb) => productFromOrder.name === productFromDb.name
+    );
 
+    const productPrice = productFromOrder.quantity * foundProduct.price;
     const productCurrency = foundProduct.currency;
-    productFromOrder.price = productFromOrder.quantity * foundProduct.price;
+    const convertedPrice = convertCurrency(
+      productPrice,
+      productCurrency,
+      orderCurrency
+    );
 
-    const convertedPrice = convertToCurrency(price, orderCurrency, productCurrency);
     productFromOrder.price = convertedPrice;
+    orderPrice = orderPrice + convertedPrice;
   }
+
+  order.price = orderPrice;
 }
+
 console.log(orders);
 console.log(products);
 
-function buildOrderList(orders) {
+buildOrdersList(orders);
+
+function buildOrdersList(orders) {
   for (const order of orders) {
-    const tr = document.createElement("tr");
+    const orderRow = document.createElement("tr");
+
     const id = document.createElement("td");
-    const state = document.createElement("td");
-    const product = document.createElement("td");
+    const status = document.createElement("td");
+    const price = document.createElement("td");
 
     id.textContent = order.id;
-    state.textContent = order.status;
-    price.textContent = `${CurrencyToSymbol[order.price]} ${order.price.toFixed(2)}`;
+    price.textContent = `${
+      currencyToSymbol[order.currency]
+    } ${order.price.toFixed(2)}`;
 
-    tr.appendChild(id);
-    tr.appendChild(state);
-    tr.appendChild(product);
+    const innerStatusBadge = buildStatusBadge(order);
 
-    tableBodyNode.appendChild(tr); 
-    addProductBtn.addEventListener('click', ()=>{
-      const productSlector=document.createElement('select');
-      const productQuantity = document.createElement('input');
+    status.appendChild(innerStatusBadge);
 
-      productSlector.setAttribute('name', 'order-products');
+    orderRow.appendChild(id);
+    orderRow.appendChild(status);
+    orderRow.appendChild(price);
 
-      for (const product of products) {
-        const productOption = document.createElement('option');
-        productOption.textContent= product.name;
-        productOption.value= product.name
-
-        
-      }
-    }
-    )
-
-    newOrderForm.addEventListener('submit',(event) => {
-     event.preventDefault();
-
-   const form = new FormData (newOrderForm)
-   const orderCurrency = form.get('order-currency');
-   const orderProduct = form.getAll('order-product');
-   const body = JSON.stringify({ orderCurrency})
-
-
-   fecth ("https://116.203.151.6:3000/orders", {method: 'POST', body})}
-
-    if (order.status ==="draft") {
-    state.style.backgroundColor = "#2196f3";
-    }
-    if (order.status === "processing") {
-      state.style.backgroundColor = "#ffc107";
-    }
-    if (order.status === "shipped") {
-      state.style.backgroundColor = "#4caf50";
-    }
-    if (order.status === "cancelled") {
-      state.style.backgroundColor = "#ff5722";
-    }
-    const ul = document.createElement("ul");
-
-    for (const product of order.products) {
-      const li = document.createElement("li");
-      li.textContent = product;
-      ul.appendChild(li);
-    }
-    product.appendChild(ul);
+    tableBodyNode.appendChild(orderRow);
   }
 }
 
-function convertToCurrency(value, fromCurrency, toCurrency) {
-  const conversionRate = currencyValues[toCurrency] / currencyValues[fromCurrency];
+addProductBtn.addEventListener("click", () => {
+  const productSelect = document.createElement("select");
+  const productQuantity = document.createElement("input");
 
-  const convertedVlaue = value * conversionRate;
+  productSelect.setAttribute("name", "order-products");
+  productQuantity.setAttribute("name", "products-quantity");
 
-  return convertedVlaue;
+  for (const product of products) {
+    const productOption = document.createElement("option");
+    productOption.textContent = product.name;
+    productOption.value = product.name;
+
+    productSelect.appendChild(productOption);
+  }
+
+  productsForm.appendChild(productSelect);
+  productsForm.appendChild(productQuantity);
+});
+
+newOrderForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const form = new FormData(newOrderForm);
+
+  const orderCurrency = form.get("order-currency");
+  const orderProducts = form.getAll("order-products");
+  const productQuantities = form.getAll("products-quantity");
+
+  const body = JSON.stringify({
+    currency: orderCurrency,
+    products: orderProducts.map((op, index) => {
+      const quantity = Number.parseInt(productQuantities[index]);
+
+      return { name: op, quantity };
+    }),
+  });
+
+  fetch("http://116.203.151.6:3000/orders", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body,
+  })
+    .then(() => {
+      return fetch("http://116.203.151.6:3000/orders");
+    })
+    .then((res) => {
+      return res.json();
+    })
+    .then((orders) => {
+      buildOrdersList(orders);
+    });
+});
+
+function buildStatusBadge(order) {
+  const innerStatusBadge = document.createElement("div");
+  innerStatusBadge.classList.add("status-badge");
+  innerStatusBadge.textContent = `${order.status
+    .charAt(0)
+    .toUpperCase()}${order.status.slice(1)}`;
+
+  switch (order.status) {
+    case "processing":
+      innerStatusBadge.classList.add("status-warning");
+      break;
+    case "shipped":
+      innerStatusBadge.classList.add("status-success");
+      break;
+    case "cancelled":
+      innerStatusBadge.classList.add("status-error");
+      break;
+    default:
+      innerStatusBadge.classList.add("status-info");
+  }
+
+  return innerStatusBadge;
+}
+
+function convertCurrency(value, fromCurrency, toCurrency) {
+  const conversionRate =
+    currencyValues[toCurrency] / currencyValues[fromCurrency];
+  const convertedValue = value * conversionRate;
+
+  return convertedValue;
 }
